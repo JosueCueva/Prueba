3777df46984215c1d051636b3077a4f301991244
Rodrigo Tufiño <rtufino@ups.edu.ec>
2021-06-30 08:47:10 -0500

    Cambio final

--- prueba.cpp
+++ prueba.cpp
@@ -6,12 +6,8 @@ int main()
 {
 	
 	cout << "Hola mundo" << endl;
-<<<<<<< HEAD:SinNombre1.cpp
 	cout << "Cualquier cosa" << endl;
-	
-=======
-	// Esto lo agrego Rodrigo.
 	cout << "Probando desde mi computador" << endl;
->>>>>>> 8ed2c7a6fa905810f2b8cc546d685a0289cd33e2:prueba.cpp
+	cout << "Con esto finaliza la clase de Git" << endl;
 	return 0;
 }
